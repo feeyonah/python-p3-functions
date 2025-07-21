9e558ed5b2e9549047118d910edf18ab2af633fb
fiona <omburafiona@gmail.com>
2025-07-21 06:11:06 +0300

    runnind python code

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Maureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(9,7))
 
 def halve(number):
     pass
